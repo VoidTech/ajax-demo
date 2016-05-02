945a97f3fdec2bea7be079c86b333e1735a146b7
VoidTech <wojciech.wysocki96@gmail.com>
2016-05-02 11:37:33 +0200

    Created ajax.js

--- ajax.js
+++ ajax.js
@@ -1,1 +1,15 @@
-//JAvaScript med Ajax
+//JavaScript med Ajax
+// Vi ska använda AJAX för att hämta innehållet på en hemsida
+
+// teknik för att hämnta data från url:er
+// AJAX - Asynchronous JavaScript and Xml
+
+//Påbörja en hämtning
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function()
+{
+	console.log("Någonting hände!");
+}
+
+requst.open('GET',"http://www.google.com");
