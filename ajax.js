c44e7c7b4add5c30713f2ffc9561e3b855b2b83d
VoidTech <wojciech.wysocki96@gmail.com>
2016-05-02 11:44:52 +0200

    Created ajax.js

--- ajax.js
+++ ajax.js
@@ -10,6 +10,7 @@ var request = new XMLHttpRequest();
 request.onreadystatechange = function(req, res)
 {
 	console.log("Någonting hände!");
+    console.log("Svaret (response) är" + res.responseText);
 }
 
 request.open('GET','http://mardby.se/AJK15G/lorem_text.php');
